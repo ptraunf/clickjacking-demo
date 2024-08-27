846c45fc996c294392da53e37a7849443a81aebc
pete <153457778+ptraunf@users.noreply.github.com>
2024-08-27 14:47:31 -0400

    corrected prod target url

--- webpack.attacker.js
+++ webpack.attacker.js
@@ -9,7 +9,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
 function getTargetIframeSrc(env) {
     const LOCAL_TARGET_URL = "http://localhost:3333/target.html";
-    const PROD_TARGET_URL = "https://clickjack-target.peter_it_guy.pages.dev/target.html";
+    const PROD_TARGET_URL = "https://clickjack-target.pages.dev/target.html";
     switch (env) {
         case "development":
             return LOCAL_TARGET_URL;
